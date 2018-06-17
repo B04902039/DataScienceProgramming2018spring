ca3ff1526c36e6fa7760b1dcff80d5462ec6f2c6
B04902039 <b04902039@ntu.edu.tw>
2018-06-17 14:52:09 +0800

    add model

--- final/model.py
+++ final/model.py
@@ -12,3 +12,17 @@ from sklearn import linear_model, metrics, model_selection, svm, preprocessing, 
 df = pd.read_csv('raw_data.csv', encoding='big5')
 df.drop(['section', 'address', 'date', 'built_date'], inplace=True, axis=1)
 df.dropna(inplace=True)
+
+x = df.drop('unit_PRICE', axis=1).values
+y = df.unit_PRICE.values
+
+# categorize data
+le = preprocessing.LabelEncoder()
+for i in [14, 15, 16]:
+    x[:, i] = le.fit_transform(x[:, i])
+    
+# models
+RF = ensemble.RandomForestRegressor(random_state=9487)
+result_RF = model_selection.cross_validate(RF, x, y, cv=5, n_jobs=-1, scoring='neg_mean_squared_error', return_train_score=True)
+print('5-fold validation acc:', result_RF['test_score'].mean())
+result_RF
